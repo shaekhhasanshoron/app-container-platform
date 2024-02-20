18f5c0ba0ad084f7fb04004136ea19133b8c1ec2
shaekhhasanshoron <shaekh.shoron@klovercloud.com>
2024-02-20 17:40:32 +0600

    Rabbit MQ updated

--- api/common.go
+++ api/common.go
@@ -3,6 +3,7 @@ package api
 import (
 	"app-container-platform/db/cp_rabbitmq"
 	"app-container-platform/db/cp_redis"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
@@ -16,24 +17,37 @@ type commonApi struct {
 }
 
 func (c2 commonApi) PullFromRedisAndPublishToRabbitMQ(c echo.Context) error {
+	queue := c.QueryParam("queue")
+	if queue == "" {
+		queue = "default"
+	}
+
 	keys, err := cp_redis.GetAllKeys()
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, "error getting key from redis")
 	}
-	var dataList []string
+
+	go syncToQueue(keys, queue)
+	return c.JSON(http.StatusOK, "sync has been started")
+}
+
+func syncToQueue(keys []string, queue string) {
+	log.Println("Sync has been started; Queue - " + queue)
+
 	for i, key := range keys {
-		data, _ := cp_redis.Get(key)
-		log.Println("data from redis")
-		log.Printf("%v :key: %v: %v\n", i, key, data)
-		dataList = append(dataList, data)
-	}
-	for _, message := range dataList {
-		err := cp_rabbitmq.PublishToRabbitMQ("default", message)
+		data, err := cp_redis.Get(key)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, "error publishing in rabbitmq")
+			log.Println(fmt.Sprintf("[ERROR] sync error - gettting data for key '%s': %s", key, err.Error()))
+			continue
+		}
+		log.Printf("data from redis -- %v :key: %v: %v\n", i, key, data)
+
+		err = cp_rabbitmq.PublishToRabbitMQ(queue, data)
+		if err != nil {
+			log.Println(fmt.Sprintf("[ERROR] sync error - publishing in rabbitmq for key '%s': %s", key, err.Error()))
+			continue
 		}
 	}
-	return c.JSON(http.StatusOK, "operation successful")
 }
 
 func CommonApi() CommonAPiInf {
