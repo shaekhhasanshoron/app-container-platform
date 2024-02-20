18fd05f3af2043e3cf892f72bd6d6d61a5cf265e
shaekhhasanshoron <shaekh.shoron@klovercloud.com>
2024-02-20 17:10:22 +0600

    Rabbit MQ updated

--- db/cp_rabbitmq/db.go
+++ db/cp_rabbitmq/db.go
@@ -2,50 +2,43 @@ package cp_rabbitmq
 
 import (
 	"app-container-platform/config"
-	"errors"
 	"fmt"
 	"github.com/streadway/amqp"
 	"log"
 )
 
-type RabbitMQClient struct {
-	Connection *amqp.Connection
-	Channel    *amqp.Channel
-}
+//type RabbitMQClient struct {
+//	Connection *amqp.Connection
+//	Channel    *amqp.Channel
+//}
+//
+//var rmq *RabbitMQClient
 
-var rmq *RabbitMQClient
+// var rmqConnection *amqp.Connection
+var rmqChannel *amqp.Channel
 
 func InitRabbitMQConnection() error {
-	rmq, err := connectToRabbitMQ(config.RabbitMQConnectionUrl)
+	// Connect to RabbitMQ
+	conn, err := amqp.Dial(config.RabbitMQConnectionUrl)
 	if err != nil {
-		log.Fatalf("Error connecting to RabbitMQ: %v", err)
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
 	}
-	defer rmq.Connection.Close()
-	defer rmq.Channel.Close()
-	fmt.Println("Connected to RabbitMQ!")
+	defer conn.Close()
+
+	// Open a channel
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+	rmqChannel = ch
+
+	log.Println("Connected to RabbitMQ!")
 	return nil
 }
 
-func connectToRabbitMQ(url string) (*RabbitMQClient, error) {
-	conn, err := amqp.Dial(url)
-	if err != nil {
-		return nil, fmt.Errorf("error connecting to RabbitMQ: %v", err)
-	}
-
-	ch, err := conn.Channel()
-	if err != nil {
-		conn.Close()
-		return nil, fmt.Errorf("error opening RabbitMQ channel: %v", err)
-	}
-
-	return &RabbitMQClient{
-		Connection: conn,
-		Channel:    ch,
-	}, nil
-}
-
 func PublishToRabbitMQ(queueName, message string) error {
-	err := rmq.Channel.Publish(
+	err := rmqChannel.Publish(
 		"",        // exchange
 		queueName, // routing key
 		false,     // mandatory
@@ -55,14 +48,14 @@ func PublishToRabbitMQ(queueName, message string) error {
 			Body:        []byte(message),
 		})
 	if err != nil {
-		return errors.New("error publishing message to RabbitMQ: %v")
+		return fmt.Errorf("error publishing message to RabbitMQ: %v", err)
 	}
 	return nil
 }
 
 func ConsumeFromRabbitMQ(queueName string) (<-chan amqp.Delivery, error) {
 	log.Println("consume 1")
-	msgs, err := rmq.Channel.Consume(
+	msgs, err := rmqChannel.Consume(
 		queueName, // queue
 		"",        // consumer
 		true,      // auto-ack
